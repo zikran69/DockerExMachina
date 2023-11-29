6a8bdd9a9491e14340b778c028be157306720b25
zikran69 <zikzigi69@gmail.com>
2023-11-30 06:30:13 +0700

    add body & menu card

---
+++ src/component/Body/Foodcard.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+const foodItem = [
+  {
+    img: "./src/assets/cake.jpg",
+    text: "Cake",
+  },
+  {
+    img: "./src/assets/fries.jpg",
+    text: "Fries",
+  },
+  {
+    img: "./src/assets/noodle.jpg",
+    text: "Noodle",
+  },
+];
+
+export default function Foodcard() {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const nextSlide = () => {
+    setActiveSlide((prevSlide) =>
+      prevSlide === foodItem.length - 1 ? 0 : prevSlide + 1,
+    );
+  };
+
+  const prevSlide = () => {
+    setActiveSlide((prevSlide) =>
+      prevSlide === 0 ? foodItem.length - 1 : prevSlide - 1,
+    );
+  };
+
+  return (
+    <div className="bg-white w-[350px] h-[300px] rounded-xl shadow-md shadow-slate-400 relative overflow-hidden">
+      {/* Carousel */}
+      <div
+        className="flex transition-transform ease-in-out duration-300"
+        style={{
+          transform: `translateX(-${activeSlide * 100}%)`,
+          height: "100%",
+        }}
+      >
+        {foodItem.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center px-4"
+            style={{ flex: "0 0 100%" }}
+          >
+            <div className="pt-2">
+              <img
+                className="w-[220px] h-[220px] rounded-[40px] brightness-75 shadow-amber-700 shadow-md"
+                src={item.img}
+                alt=""
+              />
+            </div>
+            <div className="bg-amber-700 font-secondary font-bold text-[20px] p-2 mt-2 rounded-2xl shadow-amber-700 shadow-md">
+              {item.text}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Tombol Next dan Prev */}
+      <button
+        onClick={prevSlide}
+        className="text-[40px] text-slate-800 ri-arrow-left-s-line absolute top-1/2 left-2 transform -translate-y-1/2"
+      ></button>
+      <button
+        onClick={nextSlide}
+        className="text-[40px] text-slate-800 ri-arrow-right-s-line absolute top-1/2 right-2 transform -translate-y-1/2"
+      ></button>
+    </div>
+  );
+}
--- src/component/home/Body.jsx
+++ src/component/home/Body.jsx
@@ -1,7 +1,16 @@
+import Drinkcard from "../Body/Drinkcard";
+import Foodcard from "../Body/Foodcard";
+
 export default function Body() {
   return (
-    <div className="text-[50px] bg-emerald-600 text-white pl-2 font-secondary">
-      Body
+    <div className="grid grid-flow-cols p-2 lg:p-4 justify-center items-center pt-[5px] lg:pt-[15px] border-4 border-amber-700 m-2 lg:m-6 rounded-lg">
+      <h1 className="font-primary text-stone-700 text-[30px] lg:text-[50px] px-[90px] lg:px-[270px] py-2 ">
+        Menu Kami
+      </h1>
+      <div className="flex flex-col lg:flex-row gap-6 lg:gap-[150px] justify-between">
+        <Drinkcard />
+        <Foodcard />
+      </div>
     </div>
   );
 }
